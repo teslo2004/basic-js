694dfe9d0fd5908acbae85758044cf8e1d13ec51
teslo2004 <36759305+teslo2004@users.noreply.github.com>
2020-03-18 23:44:43 +0200

    feat: add solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,5 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    var turns = 2 ** disksNumber - 1;
+    return {turns,
+         seconds: turns/(turnsSpeed/3600)}
 }
